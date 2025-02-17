09cf0fd98e0863a911af64f54645f5b152e03a9b
skjihadhasan360 <skjihadhasan360@gmail.com>
2025-02-17 08:37:08 +0600

    script.js

--- script.js
+++ script.js
@@ -1,117 +1,40 @@
-/* ----- NAVIGATION BAR FUNCTION ----- */
-    function myMenuFunction(){
-      var menuBtn = document.getElementById("myNavMenu");
-
-      if(menuBtn.className === "nav-menu"){
-        menuBtn.className += " responsive";
-      } else {
-        menuBtn.className = "nav-menu";
-      }
+function hamburg(){
+    const navbar = document.querySelector(".dropdown")
+    navbar.style.transform = "translateY(0px)"
+}
+function cancel(){
+    const navbar = document.querySelector(".dropdown")
+    navbar.style.transform = "translateY(-500px)"
+}
+// Typewriter Effect
+const texts = [
+    "DEVELOPER",
+    "DESIGNER",
+    "YOUTUBER"
+]
+let speed  =100;
+const textElements = document.querySelector(".typewriter-text");
+let textIndex = 0;
+let charcterIndex = 0;
+function typeWriter(){
+    if (charcterIndex < texts[textIndex].length){
+        textElements.innerHTML += texts[textIndex].charAt(charcterIndex);
+        charcterIndex++;
+        setTimeout(typeWriter, speed);
     }
-
-/* ----- ADD SHADOW ON NAVIGATION BAR WHILE SCROLLING ----- */
-    window.onscroll = function() {headerShadow()};
-
-    function headerShadow() {
-      const navHeader =document.getElementById("header");
-
-      if (document.body.scrollTop > 50 || document.documentElement.scrollTop >  50) {
-
-        navHeader.style.boxShadow = "0 1px 6px rgba(0, 0, 0, 0.1)";
-        navHeader.style.height = "70px";
-        navHeader.style.lineHeight = "70px";
-
-      } else {
-
-        navHeader.style.boxShadow = "none";
-        navHeader.style.height = "90px";
-        navHeader.style.lineHeight = "90px";
-
-      }
+    else{
+        setTimeout(eraseText, 1000)
     }
-
-
-/* ----- TYPING EFFECT ----- */
-   var typingEffect = new Typed(".typedText",{
-      strings : ["Designer","Youtuber","Developer"],
-      loop : true,
-      typeSpeed : 100, 
-      backSpeed : 80,
-      backDelay : 2000
-   })
-
-
-/* ----- ## -- SCROLL REVEAL ANIMATION -- ## ----- */
-   const sr = ScrollReveal({
-          origin: 'top',
-          distance: '80px',
-          duration: 2000,
-          reset: true     
-   })
-
-  /* -- HOME -- */
-  sr.reveal('.featured-text-card',{})
-  sr.reveal('.featured-name',{delay: 100})
-  sr.reveal('.featured-text-info',{delay: 200})
-  sr.reveal('.featured-text-btn',{delay: 200})
-  sr.reveal('.social_icons',{delay: 200})
-  sr.reveal('.featured-image',{delay: 300})
-  
-
-  /* -- PROJECT BOX -- */
-  sr.reveal('.project-box',{interval: 200})
-
-  /* -- HEADINGS -- */
-  sr.reveal('.top-header',{})
-
-/* ----- ## -- SCROLL REVEAL LEFT_RIGHT ANIMATION -- ## ----- */
-
-  /* -- ABOUT INFO & CONTACT INFO -- */
-  const srLeft = ScrollReveal({
-    origin: 'left',
-    distance: '80px',
-    duration: 2000,
-    reset: true
-  })
-  
-  srLeft.reveal('.about-info',{delay: 100})
-  srLeft.reveal('.contact-info',{delay: 100})
-
-  /* -- ABOUT SKILLS & FORM BOX -- */
-  const srRight = ScrollReveal({
-    origin: 'right',
-    distance: '80px',
-    duration: 2000,
-    reset: true
-  })
-  
-  srRight.reveal('.skills-box',{delay: 100})
-  srRight.reveal('.form-control',{delay: 100})
-  
-
-
-/* ----- CHANGE ACTIVE LINK ----- */
-  
-  const sections = document.querySelectorAll('section[id]')
-
-  function scrollActive() {
-    const scrollY = window.scrollY;
-
-    sections.forEach(current =>{
-      const sectionHeight = current.offsetHeight,
-          sectionTop = current.offsetTop - 50,
-        sectionId = current.getAttribute('id')
-
-      if(scrollY > sectionTop && scrollY <= sectionTop + sectionHeight) { 
-
-          document.querySelector('.nav-menu a[href*=' + sectionId + ']').classList.add('active-link')
-
-      }  else {
-
-        document.querySelector('.nav-menu a[href*=' + sectionId + ']').classList.remove('active-link')
-
-      }
-    })
-  }
-
-  window.addEventListener('scroll', scrollActive)
+}
+function eraseText(){
+    if(textElements.innerHTML.length > 0){
+        textElements.innerHTML = textElements.innerHTML.slice(0,-1);
+        setTimeout(eraseText, 50)
+    }
+    else{
+        textIndex = (textIndex + 1) % texts.length;
+        charcterIndex = 0;
+        setTimeout(typeWriter, 500)
+    }
+}
+window.onload = typeWriter
